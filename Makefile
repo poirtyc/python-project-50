dda7d8a91a68d2c66719dbeb8f4e62cf397112e0
Dmitriy Boyko <Dmitriyboyko19@gmail.com>
2025-01-12 23:43:27 +0500

    added test

--- Makefile
+++ Makefile
@@ -1,8 +1,14 @@
 install:
 	uv sync
 
+run:
+	uv run hexlet-code
+
 test:
 	uv run pytest
+
+test-cov:
+	uv run pytest --cov=gendiff --cov-report=term-missing
 
 lint:
 	uv run ruff check
--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,9 +1,31 @@
-# import pytest
-# from pathlib import Path
-# from gendiff.scripts.generate_diff import generate_diff
-#
-# def get_path_files(filename):
-#     return Path(__file__).parent / 'test_data' / filename
-#
-# def read_file(filename):
-#     return get_path_files(filename).read_text()
+from pathlib import Path
+
+import pytest
+
+from gendiff.scripts.generate_diff import generate_diff
+
+
+@pytest.fixture
+def generate_diff_func():
+    return generate_diff
+
+
+def get_path_files(filename):
+    return Path(__file__).parent / 'test_data' / filename
+
+
+def read_file(filename):
+    try:
+        return get_path_files(filename).read_text()
+    except FileNotFoundError:
+        pytest.fail(f"File {filename} not found")
+
+
+def test_generate_diff(generate_diff_func):
+    file1 = get_path_files("file1.json")
+    file2 = get_path_files("file2.json")
+    after_file = read_file("after.txt")
+
+    actual = generate_diff_func(str(file1), str(file2))
+
+    assert actual == after_file
