4e4546f6cf2eab95a3f9e301d13aa39eee6daa03
Dmitriy Boyko <Dmitriyboyko19@gmail.com>
2025-01-12 22:48:42 +0500

    make lint

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,5 @@
 import argparse
+
 from .generate_diff import generate_diff
 
 
@@ -6,7 +7,9 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("first_file")
     parser.add_argument("second_file")
-    parser.add_argument("-f", "--format", help="set format of output", metavar="FORMAT")
+    parser.add_argument(
+        "-f", "--format", help="set format of output", metavar="FORMAT"
+    )
     args = parser.parse_args()
     data = generate_diff(args.first_file, args.second_file)
     print(data)
---
+++ tests/test_generate_diff.py
@@ -0,0 +1,9 @@
+# import pytest
+# from pathlib import Path
+# from gendiff.scripts.generate_diff import generate_diff
+#
+# def get_path_files(filename):
+#     return Path(__file__).parent / 'test_data' / filename
+#
+# def read_file(filename):
+#     return get_path_files(filename).read_text()
