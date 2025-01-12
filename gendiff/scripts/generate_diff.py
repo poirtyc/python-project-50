ae790925eb6886eb0843c2003d75a259940b51f2
Dmitriy Boyko <Dmitriyboyko19@gmail.com>
2025-01-13 00:00:50 +0500

    refactoring code on generate_diff

--- gendiff/scripts/generate_diff.py
+++ gendiff/scripts/generate_diff.py
@@ -10,22 +10,27 @@ def compare_keys(data1, data2):
     return sorted(data1.keys() | data2.keys())
 
 
+def format_diff(key, data1, data2):
+    if key in data1 and key in data2:
+        if data1[key] == data2[key]:
+            return f"  {key}: {data1[key]}"
+        return [f"- {key}: {data1[key]}", f"+ {key}: {data2[key]}"]
+    elif key in data1:
+        return f"- {key}: {data1[key]}"
+    else:
+        return f"+ {key}: {data2[key]}"
+
+
 def generate_diff(file1_path, file2_path):
     data1 = load_data(file1_path)
     data2 = load_data(file2_path)
 
     result = []
-    keys = compare_keys(data1, data2)
-
-    for key in keys:
-        if key in data1 and key not in data2:
-            result.append(f"- {key}: {data1[key]}")
-        elif key in data2 and key not in data1:
-            result.append(f"+ {key}: {data2[key]}")
-        elif data1[key] == data2[key]:
-            result.append(f"  {key}: {data1[key]}")
+    for key in compare_keys(data1, data2):
+        formatted = format_diff(key, data1, data2)
+        if isinstance(formatted, list):
+            result.extend(formatted)
         else:
-            result.append(f"- {key}: {data1[key]}")
-            result.append(f"+ {key}: {data2[key]}")
+            result.append(formatted)
 
     return "{\n  " + "\n  ".join(result) + "\n}"
