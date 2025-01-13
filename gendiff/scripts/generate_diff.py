3b6e845fd46221e240755823fb08239a97e08a57
Dmitriy Boyko <Dmitriyboyko19@gmail.com>
2025-01-13 11:35:11 +0500

    refactoring code on generate_diff

--- gendiff/scripts/generate_diff.py
+++ gendiff/scripts/generate_diff.py
@@ -10,22 +10,21 @@ def compare_keys(data1, data2):
     return sorted(data1.keys() | data2.keys())
 
 
+def format_diff(key, data1, data2):
+    if key in data1 and key not in data2:
+        return f"- {key}: {data1[key]}"
+    if key in data2 and key not in data1:
+        return f"+ {key}: {data2[key]}"
+    if data1[key] == data2[key]:
+        return f"  {key}: {data1[key]}"
+    return f"- {key}: {data1[key]}\n  + {key}: {data2[key]}"
+
+
 def generate_diff(file1_path, file2_path):
     data1 = load_data(file1_path)
     data2 = load_data(file2_path)
 
-    result = []
     keys = compare_keys(data1, data2)
-
-    for key in keys:
-        if key in data1 and key not in data2:
-            result.append(f"- {key}: {data1[key]}")
-        elif key in data2 and key not in data1:
-            result.append(f"+ {key}: {data2[key]}")
-        elif data1[key] == data2[key]:
-            result.append(f"  {key}: {data1[key]}")
-        else:
-            result.append(f"- {key}: {data1[key]}")
-            result.append(f"+ {key}: {data2[key]}")
+    result = [format_diff(key, data1, data2) for key in keys]
 
     return "{\n  " + "\n  ".join(result) + "\n}"
